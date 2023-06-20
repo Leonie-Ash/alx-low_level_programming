9e50e8884a4f7dcb2c74d2ab65d8f7847cad054e
Leonie-Ash <leoniemasakha@gmail.com>
2023-06-20 09:33:11 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 
+ * print_alphabet_x10 - Make alphabet x10
  * Return: void
  */
 
 void print_alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
