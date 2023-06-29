144f8b799a616a1ca126bf5b941619269ec6a3fd
Leonie-Ash <leoniemasakha@gmail.com>
2023-06-29 07:52:41 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 	int i;
 	int t;
 
-	for (i =0; i < n--; i++)
+	for (i = 0; i < n--; i++)
 	{
-		t =a[i];
-		a[i] = a [n];
+		t = a[i];
+		a[i] = a[n];
 		a[n] = t;
 	}
 }
