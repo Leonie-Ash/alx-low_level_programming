342ce5faa4ee6feee7be7e41aeb481f5d459ec1f
Leonie-Ash <leoniemasakha@gmail.com>
2023-07-10 10:05:32 -0700

    valgrind

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - prints grid of integers
+ * @grid: address of two dimensional grid
+ * @height: height of grid
+ * Return: 0
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
