c71904bcc221b2a44921cb93ee0836f2d10986dd
Leonie-Ash <leoniemasakha@gmail.com>
2023-07-17 07:48:15 -0700

    poppy

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,11 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to be printed out
- * Return: 1 on success, -1 on error and error number is appropriately
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-
 {
 	return (write(1, &c, 1));
 }
